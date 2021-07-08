8ab2d7bc8126f188071305e921c6836b92b62c8f
Camilo Santana Melgaço <camilocsm@gmail.com>
2021-07-08 18:58:38 -0300

    improved lexical tester code

--- compiler.cpp
+++ compiler.cpp
@@ -14,24 +14,20 @@ int main(int argc, char* argv[]){
 
         // O código a seguir é usado apenas para testar o analisador léxico.
         // TODO: depois de pronto, comentar o código abaixo.
-        Lexeme lex;
-        while((lex = lexer.nextToken()).type > 0) {
-            printf("(\"%s\", %s)\n", lex.token.c_str(), tt2str(lex.type).c_str());
-        }
-        switch (lex.type) {
-            case TKN_INVALID_TOKEN:
+        Lexeme lex = lexer.nextToken();
+        while(lex.type != TKN_END_OF_FILE && lex.type != TKN_UNEXPECTED_EOF){
+            if(lex.type==TKN_INVALID_TOKEN)
                 printf("Linha %02d: Lexema inválido [%s]\n", lexer.line(), lex.token.c_str());
-                break;
-            case TKN_UNEXPECTED_EOF:
-                printf("Linha %02d: Fim de arquivo inesperado\n", lexer.line());
-                break;
-            default:
-                printf("(\"%s\", %s)\n", lex.token.c_str(), tt2str(lex.type).c_str());
-                break;
+            printf("(\"%s\", %s)\n", lex.token.c_str(), tt2str(lex.type).c_str());
+            lex = lexer.nextToken();
         }
+        printf("(\"%s\", %s)\n", lex.token.c_str(), tt2str(lex.type).c_str());
+        if(lex.type==TKN_UNEXPECTED_EOF)
+            printf("Linha %02d: Fim de arquivo inesperado\n", lexer.line());
         printf("Linhas: %d\n", lexer.line());
         // Comentar até aqui
-    } catch(const std::string& error){
+    }
+    catch(const std::string& error){
         std::cerr << error << std::endl;
     }
 
